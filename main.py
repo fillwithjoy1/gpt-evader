5c7fea99b5f376622e7d46e2d325a1183169c7bd
joysi <joysingh.js1@gmail.com>
2023-05-02 11:57:59 +1000

    Backbone commit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import time
+
 import openai
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 openai.api_key = open('env', 'r').read()
 
@@ -9,4 +14,23 @@ completion = openai.ChatCompletion.create(
     ]
 )
 
-print(completion.choices[0].message['content'])
+response = completion.choices[0].message['content']
+print(response)
+
+# Open browser and navigate to zerogpt.com
+driver = webdriver.Edge()
+driver.get("https://zerogpt.com")
+
+# Give time for Edge to finish loading
+driver.implicitly_wait(10)
+
+# Find the text area and dump the OpenAI response
+text_area = driver.find_element(by=By.ID, value="textArea")
+text_area.send_keys(response)
+text_area.send_keys(Keys.RETURN)
+
+# Click the button with class "scoreButton"
+score_button = driver.find_element(by=By.CLASS_NAME, value="scoreButton")
+score_button.click()
+
+time.sleep(100)
